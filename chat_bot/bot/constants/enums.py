5d2d82e0d9991eb55faef3f857de87670052cd87
Simon Contreras M <simon.contreras.6@gmail.com>
2023-05-13 00:11:25 -0400

    new constants and configs for discord bot

---
+++ chat_bot/bot/constants/enums.py
@@ -0,0 +1,7 @@
+"""Enums for Discord Bot"""
+from enum import Enum
+
+
+class DefaultMessages(Enum):
+    "Default Message for Discord Bot"
+    NO_PROFILING_SET = "Hola!, creo que es primera vez que hablamos por este chat!, quieres que tome algun perfil en especifico?, reacciona a los iconos para seguir conversando :D"
