92865ab443243f14c552319266e8a597930ee0c7
Simon Contreras M <simon.contreras.6@gmail.com>
2023-05-12 22:27:10 -0400

    bot updated to work with ext.commands

--- chat_bot/bot/bot.py
+++ chat_bot/bot/bot.py
@@ -17,18 +17,9 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="$", intents=intents)
 
 
-@bot.event
-async def on_ready():
-    print(f"We have logged in as {bot.user}")
-
-
-@bot.event
-async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg)
 
 
 bot.run(os.getenv("DISCORD_BOT_TOKEN"), log_handler=None)
