10afe413c2852b5a431b81711de37e2b1d98e7a7
Simon Contreras M <simon.contreras.6@gmail.com>
2023-05-13 22:38:16 -0400

    typo fixed

--- chat_bot/bot/constants/enums.py
+++ chat_bot/bot/constants/enums.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 class DefaultMessages(Enum):
     "Default Message for Discord Bot"
-    NO_PROFILING_SET = "Hola {author_name} !, creo que es primera vez que hablamos por este chat!, Quieres que tome algun perfil en especifico?, reacciona con  ✅  para darme un perfil, sino, recciona  ❌  para continuar conversando ..."
+    NO_PROFILING_SET = "Hola {author_name} !, creo que es primera vez que hablamos por este chat!, Quieres que tome algun perfil en especifico?, reacciona con  ✅  para darme un perfil, sino, reacciona  ❌  para continuar conversando ..."
     REACT_TO_MESSAGE_OTHERWISE_BLOCK = "Por favor reacciona a este mensaje con  ✅  o con  ❌ para poder continuar con nuestra conversacion."
 
 
